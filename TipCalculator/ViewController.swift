ef2200ba32f4d042f94db09bf1c51442ec9207e2
mendybarouk <mendibarouk@gmail.com>
2017-03-21 12:47:47 +0200

    init

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var textFieldPrice: UITextField!
+    @IBOutlet weak var labelTip: UILabel!
+    @IBOutlet weak var labelTotalPrice: UILabel!
+    @IBOutlet weak var SCTipPercentage: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        SCTipPercentage.addTarget(self, action: #selector(calculatePrice), for: .valueChanged)
+        textFieldPrice.addTarget(self, action: #selector(calculatePrice), for: .editingChanged)
+    }
+    
+    //MARK: --My Function--
+    func calculatePrice() {
+        var tipPercent: Double = 0
+        switch SCTipPercentage.selectedSegmentIndex {
+        case 0: tipPercent = 0.1
+        case 1: tipPercent = 0.15
+        case 2: tipPercent = 0.2
+        default:
+            print("No selected segment")
+        }
+        displayPrices(tipPercent: tipPercent)
+    }
+    
+    func displayPrices(tipPercent: Double) {
+        
+        guard textFieldPrice.text != nil && (Double(textFieldPrice.text!) != nil) && !(textFieldPrice.text?.isEmpty)! else {
+            labelTip.text = "₪ Tip"
+            labelTotalPrice.text = "₪ Total"
+            return
+        }
+        
+        let price: Double = Double(textFieldPrice.text!)!
+        let tip: Double = tipPercent * price
+
+        labelTip.text = "₪ " + String(format: "%.2f", tip)
+        labelTotalPrice.text = "₪ " + String(format: "%.2f", tip + price)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
-
 
 }
 
